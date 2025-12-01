3f9f521ecc9e884b970c95a0451080bdf929655f
Your Name <you@example.com>
2025-12-01 13:19:32 +0100

    update

--- pages/[[...catchall]].tsx
+++ pages/[[...catchall]].tsx
@@ -61,12 +61,13 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const pageModules = await PLASMIC.fetchPages();
+
   return {
-    paths: pageModules.map((mod) => ({
+    paths: pageModules.map((mod: { path: string }) => ({
       params: {
         catchall: mod.path.substring(1).split("/"),
       },
     })),
     fallback: "blocking",
   };
-}
+};
